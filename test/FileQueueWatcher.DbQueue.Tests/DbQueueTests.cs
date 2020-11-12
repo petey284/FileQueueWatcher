3b0dd0915a4b0a86815fce260f82bb20f181be66
Peterson Onyeukwu <10350886+petey284@users.noreply.github.com>
2020-11-12 12:17:08 -0500

    Adds more coverage for DbQueue and Database classes

--- test/FileQueueWatcher.DbQueue.Tests/DbQueueTests.cs
+++ test/FileQueueWatcher.DbQueue.Tests/DbQueueTests.cs
@@ -1,13 +1,52 @@
 using System;
+using SQLite.Net;
 using Xunit;
 
-namespace FileQueueWatcher.DbQueue.Tests
+namespace FileQueueWatcher.Tests
 {
     public class DbQueueTests
     {
         [Fact]
         public void InitConstructorTests()
         {
+            var dbFilepath = "C:\\temp\\test.db";
+            var dbQueue = DbQueue.Init(dbFilepath);
+
+            Assert.Equal(dbFilepath, dbQueue.ConnectionString);
+
+            Assert.NotNull(dbQueue.Database);
+            Assert.NotNull(dbQueue);
+        }
+
+        [Fact]
+        public void InitConstructorEmptyPathThrowsErrorTests()
+        {
+            Assert.Throws<ArgumentException>(() => DbQueue.Init(string.Empty));
+        }
+
+        [Fact]
+        public void GetNewDirectoriesTests()
+        {
+            Assert.True(false);
+        }
+    }
+
+    public class DatabaseTests
+    {
+
+        [Fact]
+        public void InitialConstructorTests()
+        {
+            var database = new Database("C:\\temp\\test.db");
+
+            Assert.True(database.InitialDirectories is Table<Directory>);
+            Assert.True(database.WatchedDirectories is Table<WatchedDirectory>);
+        }
+
+        [Fact]
+        public void InitialTests()
+        {
+            Assert.Throws<ArgumentException>(() => new Database(""));
         }
     }
 }
