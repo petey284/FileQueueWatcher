d924c0415f1a52956f336a677a865a2b14d84962
Peterson Onyeukwu <10350886+petey284@users.noreply.github.com>
2020-11-12 00:16:13 -0500

    Updates to file structure

--- src/FileQueueWatcher/Program.cs
+++ src/FileQueueWatcher/Program.cs
@@ -1,12 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
-using Serilog.Formatting.Compact;
 
 namespace FileQueueWatcher
 {
