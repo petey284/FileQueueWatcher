ef0abcaaeb632373a953e2eae23a2adcf7236912
Peterson Onyeukwu <10350886+petey284@users.noreply.github.com>
2020-11-12 09:26:32 -0500

    Adds DbQueue project along with tests

---
+++ src/FileQueueWatcher.DbQueue/DbQueue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FileQueueWatcher.DbQueue
+{
+    public class DbQueue
+    {
+        public string ConnectionString;
+        public static DbQueue Init(string connectionString)
+        {
+            var dbQueue = new DbQueue();
+            dbQueue.SetConfig(connectionString);
+
+            return dbQueue;
+        }
+
+        private void SetConfig(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+    }
+};
---
+++ test/FileQueueWatcher.DbQueue.Tests/DbQueueTests.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace FileQueueWatcher.DbQueue.Tests
+{
+    public class DbQueueTests
+    {
+        [Fact]
+        public void InitConstructorTests()
+        {
+        }
+    }
+}
